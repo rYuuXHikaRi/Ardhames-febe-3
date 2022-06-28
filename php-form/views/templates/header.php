de659b63904af2ad951bbd068ee8d0ff1e8e5db4
rYuuXHikaRi <ryuux.devcode@gmail.com>
2022-06-28 17:04:32 +0700

    Fixing link directory

--- php-form/views/templates/header.php
+++ php-form/views/templates/header.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Dashboard</title>
-    <link rel="stylesheet" href="../plugins/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../plugins/css/style.css">
-    <link rel="stylesheet" href="../plugins/css/navbar.css">
-    <link rel="stylesheet" href="../plugins/css/sidebar.css">
-    <link rel="stylesheet" href="../plugins/css/dashboard.css">
+    <link rel="stylesheet" href="../../plugins/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../plugins/css/style.css">
+    <link rel="stylesheet" href="../../plugins/css/navbar.css">
+    <link rel="stylesheet" href="../../plugins/css/sidebar.css">
+    <link rel="stylesheet" href="../../plugins/css/dashboard.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
 </head>
