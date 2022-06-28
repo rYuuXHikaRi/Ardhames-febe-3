4942aaa9b162d99defa8beddde76177466e18a42
alvinyogakesuma <107390583+alvinyogakesuma@users.noreply.github.com>
2022-06-28 16:17:10 +0700

    Update register.php

--- app/views/login/register.php
+++ app/views/login/register.php
@@ -1,21 +1,50 @@
-<h1>Register Page</h1>
-
-<form type="form" action="<?= base_url; ?>/login/cekRegis" method="POST"
-enctype="multipart/form-data">
-	<label>
-		Username:
-	</label>
-	<input type="text" name="username">
-	<br>
-	<label>
-		Password
-	</label>
-	<input type="text" name="password">
-	<br>
-	<label>
-		Ulangi Password
-	</label>
-	<input type="text"  name="password2">
-	<br>
-	<button type="submit">Daftar</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style2.css" />
+    <title>Login Page</title>
+  </head>
+  <body>
+    <div class="kiri">
+      <div class="regis">
+        <h1><strong>Registration</strong></h1>
+        <p class="welcome">Silahkan isi form dibawah!</p>
+        <hr />
+        <form>
+          <label for="email">Email</label><br />
+          <input type="text" id="email" placeholder="mail@website.com" /><br />
+          <label for="password">Password</label><br />
+          <input type="password" id="password" placeholder="Min. 8 character" /><br />
+          <label for="password">Re-type Password</label><br />
+          <input type="password" id="password" placeholder="Min. 8 character" /><br />
+          <div class="name">
+            <div class="fname">
+              <label for="fname">First Name</label><br />
+              <input type="text" id="fname" placeholder="First Name" />
+            </div>
+            <div class="lname">
+              <label for="lname">Last Name</label><br />
+              <input type="text" id="lname" placeholder="Last Name" />
+            </div>
+          </div>
+          <div class="gender">
+            <p>Gender</p>
+            <div class="male">
+              <input type="radio" id="male" name="gender" />
+              <label for="male">Male</label>
+            </div>
+            <div class="female">
+              <input type="radio" id="female" name="gender" />
+              <label for="female">Female</label>
+            </div>
+          </div>
+          <input type="submit" value="Register" id="register" /><br />
+        </form>
+      </div>
+    </div>
+    <div class="kanan cf"></div>
+  </body>
+</html>
