cbcb28b73b45160a47f58fc39e1f81c792e5b92b
alvinyogakesuma <107390583+alvinyogakesuma@users.noreply.github.com>
2022-06-28 16:16:28 +0700

    Update index.php

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,16 +1,33 @@
-<h1>Login Page</h1>
-
-<form type="form" action="<?= base_url; ?>/login/cekLogin" method="POST"
-enctype="multipart/form-data">
-	<label>
-		Username:
-	</label>
-	<input type="text" name="username">
-	<br>
-	<label>
-		Password
-	</label>
-	<input type="text" name="password">
-	<br>
-	<button type="submit">Login</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Login Page</title>
+  </head>
+  <body>
+    <div class="kiri">
+      <div class="login">
+        <h1><strong>Login</strong></h1>
+        <p class="welcome">Selamat datang di Perpustakaan ITERA</p>
+        <hr>
+        <form>
+          <label for="email">Email</label><br />
+          <input type="text" id="email" placeholder="mail@website.com" /><br />
+          <label for="password">Password</label><br />
+          <input type="password" id="password" placeholder="Min. 8 character" /><br />
+          <div class="option">
+            <input type="checkbox" id="rem" />
+            <label for="rem" class="rem1">Remember me</label>
+            <a href="" class="forget">Forget password?</a><br />
+          </div>
+          <input type="submit" value="Login" id="login"/><br />
+          <label for="reg" class="reg1">Not registered yet? </label><a href="index2.html" id="reg">Create an Account</a></p>
+        </form>
+      </div>
+    </div>
+    <div class="kanan cf"></div>
+  </body>
+</html>
