aa2eb427100c696ea682e9606ed181a4fa4884db
rYuuXHikaRi <ryuux.devcode@gmail.com>
2022-06-28 16:58:29 +0700

    Recreating to PHP form

---
+++ php-form/views/dashboard/index.php
@@ -0,0 +1,52 @@
+            <!--Content-->
+            <div class="col pe-0 ps-0 content mt-10">
+                <div class="row content-padding">
+                    <div class="container mt-4 mb-3">
+                        <div class="card">
+                            <div class="card-header">
+                                <h1>Dashboard</h1>
+                            </div>
+                            <div class="card-body">
+                                <p>Panel admin untuk Buku Kita</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row content-padding"> 
+                    <div class="container mt-2">
+                        <div class="card">
+                            <div class="card-header">
+                                <h2>Database</h2>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col" style="margin-left: 200px;">
+                                        <div class="box-db">
+                                            <div class="box-db-title">
+                                                <p>Jumlah Buku</p>
+                                            </div>
+                                            <div class="box-db-number">
+                                                <p>999</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col" style="margin-right: 180px;">
+                                        <div class="box-db">
+                                            <div class="box-db-title">
+                                                <p>Jumlah Kategori</p>
+                                            </div>
+                                            <div class="box-db-number">
+                                                <p>333</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Content until here-->
